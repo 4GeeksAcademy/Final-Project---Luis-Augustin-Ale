ac862780094357f1df7a7bcfed00de042d645c86
Agus <lujan.agustin@gmail.com>
2024-10-02 17:07:54 +0200

    Subo cambios y hago pull

---
+++ dashboard_test.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import numpy as np
+import seaborn as sns
+
+st.set_page_config(
+    page_title = 'Delivery Data',
+    page_icon = "🏂",
+    layout = 'wide',
+    
+
+)
