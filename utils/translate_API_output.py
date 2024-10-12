661918acf465ba93aa3c5137ea6477f060f73519
Luis Castillo <luigercm66@gmail.com>
2024-10-12 18:46:25 +0200

    warning busqueda vacia

--- utils/translate_API_output.py
+++ utils/translate_API_output.py
@@ -1,12 +1,25 @@
 from deep_translator import GoogleTranslator
-translator = GoogleTranslator()
 #df_prueba_roberta.rename(columns={'sentiment': 'tweets'}, inplace=True)
 
 def traducir(text):
     # Detectar el idioma automáticamente y traducirlo al inglés
-    if GoogleTranslator(source='auto', target='en').translate(text):
-        return GoogleTranslator(source='auto', target='en').translate(text)
-    else:
-        return text
+    try:
+        translator = GoogleTranslator(source='auto', target='en')
+        translated_text=translator.translate(text)
+        return translated_text
+    
+    except Exception as e:
 
-df_prueba_roberta['tweets_traducidos'] = df_prueba_roberta['tweets'].apply(traducir)
+        print(f"Error al traducir el texto {e}")
+        return text
+    
+def traducir_tweets(df):
+   
+    # Asegúrate de que la columna 'tweets' existe
+    if 'Tweet' in df.columns:
+        df['Translated_tweet'] = df['Tweet'].apply(traducir)
+    else:
+        print("La columna 'Tweet' no se encontró en el DataFrame.")
+    return df
+
+# df_prueba_roberta['tweets_traducidos'] = df_prueba_roberta['tweets'].apply(traducir)
